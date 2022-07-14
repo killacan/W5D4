f4239d0630be03dfee595dce29ff01fd3b5fef93
AA Student <appacademy@Mini-18.local>
2022-07-14 16:37:55 -0700

    associations complete

--- practice-for-ch-sql-associations-long-practice-main/app/models/enrollment.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/enrollment.rb
@@ -1,2 +1,31 @@
+# == Schema Information
+#
+# Table name: enrollments
+#
+#  id         :bigint           not null, primary key
+#  course_id  :bigint           not null
+#  student_id :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Enrollment < ApplicationRecord
+
+
+
+    belongs_to :students, #kind of like a getter method 
+        primary_key: :id,
+        foreign_key: :student_id,
+        class_name: :User
+
+    belongs_to :courses,
+        primary_key: :id,
+        foreign_key: :course_id,
+        class_name: :Course
+
+
+        # #SELECT *
+        # FROM usersjoin 
+        # index_enrollments_on_ uuser.id = enrollmen.user_id
+
+
 end
--- practice-for-ch-sql-associations-long-practice-main/app/models/user.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/user.rb
@@ -1,2 +1,31 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
+
+    has_many :enrollments, 
+        primary_key: :id, 
+        foreign_key: :student_id,
+        class_name: :Enrollment
+
+    has_many :enrolled_courses,
+         through: :enrollments,
+         source: :courses
+
+    has_many :courses_to_teach,
+        primary_key: :id, 
+        foreign_key: :instructor_id, 
+        class_name: :Course
+
 end
+
+#foreign key table BELONGS TO the other table go
+#when you make a belongs to it automatically validates the foreign id
+
+#shortcut cheat:
